63eb34356c53d1d91ecc19256971e6871802255b
Joseph Kim <joekimga@gmail.com>
2021-03-18 02:00:02 -0400

    app.py day3,act10 roadmap

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,1 +1,31 @@
-scrape 
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import time
+import requests
+import pymongo
+
+def init_browser():
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+def mars_scrape():
+
+    browser = init_browser()
+
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
