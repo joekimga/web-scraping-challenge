fd58e275a8502d2162d366a67fac3b0d67410f35
Joseph Kim <joekimga@gmail.com>
2021-03-19 01:12:02 -0400

    Betsy's help

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,6 +1,7 @@
 from flask_pymongo import PyMongo
-import scrape_costa
+import scrape_mars
 from webdriver_manager.chrome import ChromeDriverManager
+from flask import Flask, render_template, redirect
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -14,11 +15,11 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 def home():
 
     # Find one record of data from the mongo database
-    mars_data = mongo.db.mars_data.find_one()
+    mars_data = mongo.db.collection.find_one()
     return render_template('index.html', mars_data = mars_data)
 
     # Return template and data
-    # return render_template("index.html", vacation=destination_data)
+
 
 
 # Route that will trigger the scrape function
@@ -26,7 +27,7 @@ def home():
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
+    mars_data = scrape_mars.mars_scrape()
 
     # Update the Mongo database using update and upsert=True
     mongo.db.collection.update({}, mars_data, upsert=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,30 +1,65 @@
 from splinter import Browser
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import requests
 import pymongo
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def init_browser():
-    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": ChromeDriverManager().install()}
     return Browser("chrome", **executable_path, headless=False)
+
 
 def mars_scrape():
 
     browser = init_browser()
 
-    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    url = "https://mars.nasa.gov/news/"
     browser.visit(url)
 
+    time.sleep(1)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    # news_title = soup.title.text.find('a', .content_title)
+    # news_paragraph = soup.body.find('p', .article_teaser_body)
+    element = soup.select_one('ul.item_list li.slide')
+
+    news_title = element.find('div', class_="content_title").get_text()
+    # news_title
+
+    news_paragraph = element.find('div', class_="article_teaser_body").get_text()
+    # news_paragraph
+
+    mars_data = {
+        "news_article": news_title,
+        "news_paragraph": news_paragraph,
+        }
+
+    # Close the browser after scraping
+    browser.quit()
+
+    # Return results
+    return mars_data
+
+
+    # html = browser.html
+    # soup = bs(html, "html.parser")
 
 
 
 
 
 
+    # news = soup.find_all('div', class_="image_and_description_container")
 
 
 
+    # featured_image_url = ('https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/' + href)
 
 
 
